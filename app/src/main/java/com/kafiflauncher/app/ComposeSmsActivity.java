4f709efa8926bea61aa38483dada130373ef643a
azdineh <azdineh@gmail.com>
2020-11-17 13:01:25 +0100

    Kafif launcher v1

---
+++ app/src/main/java/com/kafiflauncher/app/ComposeSmsActivity.java
@@ -0,0 +1,8 @@
+package com.kafiflauncher.app;
+
+import android.app.Activity;
+
+public class ComposeSmsActivity extends Activity
+	{
+    // TODO: Implement SMS/MMS compose functions and need to respond ACTION_SENDTO Intnet
+	}
---
+++ app/src/main/java/com/kafiflauncher/app/EditTextImeBackListener.java
@@ -0,0 +1,6 @@
+package com.kafiflauncher.app;
+
+public interface EditTextImeBackListener
+	{
+	public abstract void onImeBack(EditTextBackEvent ctrl, String text);
+	}
